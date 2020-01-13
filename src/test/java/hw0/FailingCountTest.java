4534906b328e36f8d9cf84239ec9817b934ae010
Zifei Wu <634610138@qq.com>
2020-01-13 14:52:00 -0800

    Three fail tests

--- src/test/java/hw0/FailingCountTest.java
+++ src/test/java/hw0/FailingCountTest.java
@@ -1,7 +1,27 @@
 package hw0;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FailingCountTest {
     // testWithFailure1
+    @Test
+    public void FirstTest(){
+        int[] a = {0,1,0};
+        int result = Count.numZero(a);
+        assertEquals(2,result);
+    }
     // testWithFailure2
+    @Test
+    public void SecondTest(){
+        int[] a = {0,1,0,0};
+        int result = Count.numZero(a);
+        assertEquals(3,result);
+    }
     // testWithFailure3
+    @Test
+    public void ThirdTest(){
+        int[] a = {0,0,0};
+        int result = Count.numZero(a);
+        assertEquals(3,result);
+    }
 }
