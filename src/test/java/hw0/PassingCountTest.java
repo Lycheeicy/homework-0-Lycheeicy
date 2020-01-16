76f0785e7f6e191925f9338f3b591b3dba9ce7e1
Zifei Wu <634610138@qq.com>
2020-01-15 19:50:04 -0800

    StatementCountTest

--- src/test/java/hw0/PassingCountTest.java
+++ src/test/java/hw0/PassingCountTest.java
@@ -1,7 +1,26 @@
 package hw0;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class PassingCountTest {
     // testWithoutFailure1
+    @Test
+    public void FirstTest(){
+        int[] a = {1,0};
+        int result = Count.numZero(a);
+        assertEquals(1,result);
+    }
     // testWithoutFailure2
+    @Test
+    public void SecondTest(){
+        int[] a = {1,0,0};
+        int result = Count.numZero(a);
+        assertEquals(2,result);
+    }
     // testWithoutFailure3
+    @Test
+    public void ThirdTest(){
+        int[] a = {3,0,0};
+        int result = Count.numZero(a);
+        assertEquals(2,result);
+    }
 }
--- src/test/java/hw0/StatementCountTest.java
+++ src/test/java/hw0/StatementCountTest.java
@@ -1,5 +1,12 @@
 package hw0;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class StatementCountTest {
     // tests that execute all statements in numZero
+    @Test
+    public void FirstTest(){
+        int[] a = {0,0,0,0};
+        int result = Count.numZero(a);
+        assertEquals(4,result);
+    }
 }
